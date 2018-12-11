589bb387b4e748204c94b5bb741d200ecbc40091
jasonyou1995 <jason.you1995@gmail.com>
2018-12-11 18:22:11 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# go_programs
+# Go_programs
--- hello.go
+++ hello.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/Jasonyou1995/stringutil"
+)
 
 func main() {
-     fmt.Printf("Hello, World!\n")
+	fmt.Printf("Hello, World!\n")
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
 }
